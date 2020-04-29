66b08fbd01e94e81e740777eb4e18f236b9bc91f
Matteo Bucalossi <mbucalossi50@gwu.edu>
2020-04-29 19:45:13 -0400

    scrapss

--- scrapers/GZ_scraper.py
+++ scrapers/GZ_scraper.py
@@ -40,3 +40,33 @@ def corpus_builder(text):
         GialloZafferano_Corpus.append(get_ingredients(recipe))
     return GialloZafferano_Corpus
 
+
+# import recipes
+recipes = ["https://ricette.giallozafferano.it/Spaghetti-alla-Carbonara.html", "https://ricette.giallozafferano.it/Spaghetti-all-Amatriciana.html",
+           "https://ricette.giallozafferano.it/Risotto-allo-Zafferano.html", "https://ricette.giallozafferano.it/Spezzatino-di-manzo.html",
+          "https://ricette.giallozafferano.it/Tiramisu.html", "https://ricette.giallozafferano.it/Pasta-al-forno.html",
+          "https://ricette.giallozafferano.it/Paccheri-ripieni-con-salsiccia-e-funghi.html", "https://ricette.giallozafferano.it/Spaghetti-allo-scoglio.html",
+          "https://ricette.giallozafferano.it/Pasta-gorgonzola-e-noci.html", "https://ricette.giallozafferano.it/Parmigiana-di-melanzane.html",
+          "https://ricette.giallozafferano.it/Polpettine-di-tonno-e-ricotta.html", "https://ricette.giallozafferano.it/Filetto-al-pepe-verde.html",
+          "https://ricette.giallozafferano.it/Zucchine-ripiene.html", "https://ricette.giallozafferano.it/Saltimbocca-alla-Romana.html",
+          "https://ricette.giallozafferano.it/Rotolo-di-frittata-farcito.html", "https://ricette.giallozafferano.it/Scaloppine-ai-funghi.html",
+          "https://ricette.giallozafferano.it/Tiramisu.html", "https://ricette.giallozafferano.it/Tortino-di-cioccolato-con-cuore-fondente.html",
+          "https://ricette.giallozafferano.it/Crema-pasticcera.html", "https://ricette.giallozafferano.it/Torta-rustica-di-mele.html",
+          "https://ricette.giallozafferano.it/Crostata-alla-frutta.html", "https://ricette.giallozafferano.it/Crostata-morbida-al-cioccolato.html",
+          "https://ricette.giallozafferano.it/Cannoli-siciliani.html", "https://ricette.giallozafferano.it/Torta-margherita.html",
+          "https://ricette.giallozafferano.it/Bigne-alla-crema.html", "https://ricette.giallozafferano.it/Taralli.html",
+          "https://ricette.giallozafferano.it/Arancini-di-riso.html", "https://ricette.giallozafferano.it/Mozzarella-in-carrozza.html",
+          "https://ricette.giallozafferano.it/Focaccia-con-pomodorini-e-origano.html", "https://ricette.giallozafferano.it/Grissini.html",
+          "https://ricette.giallozafferano.it/Frittata-di-zucchine.html", "https://ricette.giallozafferano.it/Involtini-di-melanzane.html",
+          "https://ricette.giallozafferano.it/Insalata-di-polpo-prezzemolata.html", "https://ricette.giallozafferano.it/Asparagi-in-sfoglia-con-crudo.html",
+          "https://ricette.giallozafferano.it/Carciofi-ripieni.html", "https://ricette.giallozafferano.it/Patate-al-forno.html",
+          "https://ricette.giallozafferano.it/Sformato-di-verdure.html", "https://ricette.giallozafferano.it/Verdure-gratinate-al-forno.html",
+          "https://ricette.giallozafferano.it/Peperonata.html", "https://ricette.giallozafferano.it/Cipolline-in-agrodolce.html",
+          "https://ricette.giallozafferano.it/Polpettone-alla-ligure.html", "https://ricette.giallozafferano.it/Insalata-di-ceci-gamberi-e-rucola.html",
+          "https://ricette.giallozafferano.it/Insalata-con-avocado.html", "https://ricette.giallozafferano.it/Fagiolini-alla-pugliese.html",
+          "https://ricette.giallozafferano.it/Panzerotti-calzoni-fritti.html", "https://ricette.giallozafferano.it/Treccia-di-pasta-lievitata.html",
+          "https://ricette.giallozafferano.it/Pizza-Margherita.html", "https://ricette.giallozafferano.it/Focaccia-col-formaggio.html",
+          "https://ricette.giallozafferano.it/Pane-ai-cereali.html", "https://ricette.giallozafferano.it/Risotto-alla-salsiccia.html",
+          "https://ricette.giallozafferano.it/Risotto-ai-funghi.html", "https://ricette.giallozafferano.it/Risotto-radicchio-e-pancetta.html"]
+
+corpus_builder(recipes)
--- scrapers/OT_scraper.py
+++ scrapers/OT_scraper.py
@@ -45,3 +45,23 @@ def corpus_builder(text):
         OpenTable_Corpus.append(get_menus(menu))
     return OpenTable_Corpus
 
+menus = ["https://www.opentable.com/r/crudite-milano-milan", "https://www.opentable.com/r/risotteria-italiana-milan?corrid=9cf72e53-c4ec-4acf-ba3a-6cf9a9ae8daa&p=2&sd=2020-03-31%2022%3A30",
+        "https://www.opentable.com/r/terrazza-241-como?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=eb46a391-a86e-47d7-9fd3-8e432db6fbf2&p=2&sd=2020-04-30%2023%3A00",
+        "https://www.opentable.com/r/satin-como?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=eb46a391-a86e-47d7-9fd3-8e432db6fbf2&p=2&sd=2020-04-30%2023%3A00",
+        "https://www.opentable.com/r/savo-gourmet-pizzeria-genova?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=5d8db339-e2c5-48db-bebe-cdcae29f3754&p=2&sd=2020-04-30+23%3A00",
+        "https://www.opentable.it/r/ristorante-tenuta-fasano-naples?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=55bed57d-8db4-4580-87f3-cb1b43638fc8&p=2&sd=2020-05-10%2019%3A00",
+        "https://www.opentable.it/r/le-isole-marsala?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=55bed57d-8db4-4580-87f3-cb1b43638fc8&p=2&sd=2020-05-10%2019%3A00",
+        "https://www.opentable.it/r/libra-cucina-evolution-bologna?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=d401c110-7094-45fc-82cf-843bbb04d13a&p=2&sd=2020-05-10+19%3A00",
+        "https://www.opentable.it/r/terrazza-danieli-venice?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=8d103dfc-4f8d-4066-b5e4-2231f10d6b42&p=2&sd=2020-05-22+19%3A00",
+        "https://www.opentable.it/r/giardino-dinverno-venice?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=8d103dfc-4f8d-4066-b5e4-2231f10d6b42&p=2&sd=2020-05-22+19%3A00",
+        "https://www.opentable.it/r/savo-gourmet-pizzeria-genova?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=269cccf0-9ccb-48e0-a2d1-44c91ec8ad2f&p=2&sd=2020-05-22+19%3A00",
+        "https://www.opentable.it/r/mi-garba-milan?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=c706d767-bac1-4a00-bea7-ac4856784522&p=2&sd=2020-05-22%2019%3A00",
+        "https://www.opentable.it/r/ristorante-alle-langhe-milan?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=c706d767-bac1-4a00-bea7-ac4856784522&p=2&sd=2020-05-22%2019%3A00",
+        "https://www.opentable.it/r/god-save-the-food-piave-milan?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=c706d767-bac1-4a00-bea7-ac4856784522&p=2&sd=2020-05-22%2019%3A00",
+        "https://www.opentable.it/r/osteria-della-via-appia-milano?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=c706d767-bac1-4a00-bea7-ac4856784522&p=2&sd=2020-05-22%2019%3A00",
+        "https://www.opentable.it/r/la-pizza-biscottata-gourmet-milan?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=c706d767-bac1-4a00-bea7-ac4856784522&p=2&sd=2020-05-22%2019%3A00",
+        "https://www.opentable.it/r/caffe-and-bistrot-la-locanda-del-gatto-rosso-milan?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=c706d767-bac1-4a00-bea7-ac4856784522&p=2&sd=2020-05-22+19%3A00",
+        "https://www.opentable.it/r/risotteria-italiana-milan?avt=eyJ2IjoxLCJtIjoxLCJwIjowfQ&corrid=c706d767-bac1-4a00-bea7-ac4856784522&p=2&sd=2020-05-22%2019%3A00",
+        ]
+
+corpus_builder(menus)
