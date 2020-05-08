ca785eecfeb4afe600fcd22c97295db71c7e0169
Matteo Bucalossi <mbucalossi50@gwu.edu>
2020-05-08 05:30:08 -0400

    fixes

--- analysis/scoring.py
+++ analysis/scoring.py
@@ -7,28 +7,21 @@ def scoring(result, doc):
     if result <= 5:
         scores[doc] = 5
         img = mpimg.imread('nonna-open.jpg')
-        plt.imshow(img)
-        plt.show()
     elif result >= 6 and result <= 10:
         scores[doc] = 4
         img = mpimg.imread('Red-Sauce-Raos.jpg')
-        plt.imshow(img)
-        plt.show()
     elif result >= 11 and result <= 15:
         scores[doc] = 3
         img = mpimg.imread('rosie-s-italian-grille.jpg')
-        plt.imshow(img)
-        plt.show()
     elif result >= 16 and result <= 20:
         scores[doc] = 2
         img = mpimg.imread('olive_garden.jpg')
-        plt.imshow(img)
-        plt.show()
     else:
         scores[doc] = 1
         img = mpimg.imread('pizza_deep.jpg')
-        plt.imshow(img)
-        plt.show()
+
+    plt.imshow(img)
+    plt.show()
 
     return scores
 
