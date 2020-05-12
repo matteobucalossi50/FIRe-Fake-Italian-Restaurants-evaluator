6708289128f4574538890abe3365fe1e40cdf653
Matteo Bucalossi <mbucalossi50@gwu.edu>
2020-05-12 07:27:31 -0400

    notebook

--- analysis/ling_analysis.py
+++ analysis/ling_analysis.py
@@ -1,5 +1,47 @@
 
 
+
+
+# edit distance
+def edit_dist(dic, corr_w):
+    count = 0
+    misspells = []
+    for key, value in dic.items():
+        for word in value:
+            if edit_distance(word, corr_w) < 2 and edit_distance(word, corr_w) > 0:
+                if word not in misspells:
+                    misspells.append(word)
+                    count += 1
+    return misspells, count
+
+# v expensive?
+def get_candidates(dic, words_freq):
+    missp_dic = {}
+    for w in words_freq.keys():
+        misspells, count = edit_dist(dic, w)
+        if count != 0:
+            missp_dic[w] = misspells
+
+
+# try with jaro distance
+# def jaro_dist(dic, corr_w):
+#     count = 0
+#     misspells = []
+#     for key, value in dic.items():
+#         for word in value:
+#             if jaro_similarity(word, corr_w) < 2 and jaro_similarity(word, corr_w) > 0:
+#                 misspells.append(word)
+#                 count += 1
+#     return misspells, count
+#
+# # # v expensive?
+# missp_dic = {}
+# for w in words_freq.keys():
+#     misspells, count = jaro_dist(tokens_test_s, w)
+#     missp_dic[w] = misspells
+
+
+# grams frequencies
 freq_bi = nltk.FreqDist(bigrams)
 
 print(freq_bi.most_common(20))
@@ -7,9 +49,8 @@ freq_bi.plot(10)
 
 
 '''
-print(freq_bi.hapaxes())
 print(freq_bi.N())
-print(freq_bi.freq(('financial','condition')))
+print(freq_bi.freq(('pizza', 'napoletana')))
 '''
 
 
